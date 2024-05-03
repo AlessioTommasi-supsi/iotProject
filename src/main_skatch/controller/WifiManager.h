391bab8263b73346af55b72d220580c71661e536
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2024-05-04 00:47:07 +0200

    web server funzionante!

--- src/main_skatch/controller/WifiManager.h
+++ src/main_skatch/controller/WifiManager.h
@@ -1,15 +1,17 @@
 #ifndef WiFiManager_h
 #define WiFiManager_h
 
+#include <Arduino.h>
 #include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebSrv.h>
 
 class WiFiManager
 {
-private:
-    const char *ssid;
-    const char *password;
 
 public:
+    const char *ssid;
+    const char *password;
     WiFiManager(const char *ssid, const char *password);
     WiFiManager();
     ~WiFiManager(); // Distruttore
--- src/main_skatch/main_skatch.ino
+++ src/main_skatch/main_skatch.ino
@@ -1,46 +1,39 @@
 #include <Arduino.h>
 #include "LedEsp32.h"
-#include "WifiManager.h"
 #include "WebServer.h"
 
 LedEsp32 *led;
-WiFiManager* wifiManager;
 
-WebServer server;
+const char *ssid = "HomeInternet";
+const char *password = "occhioallapennachecade";
+
+WebServer webServer(ssid, password);
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial) {
+  while (!Serial)
+  {
     ; // Attendi finché la porta seriale non è pronta
   }
-  
+
   Serial.println("La porta seriale è pronta! Hello, world!");
 
   led = new LedEsp32(13);
-  wifiManager = new WiFiManager();
-  server.begin();
+  webServer.begin();
 
   led->blink(2);
-  delay(5000);
-  led->stopBlink();
-  led->turnOn();
-  delay(50000);
-  led->turnOff();
 
   destroy();
 }
 
 void destroy()
 {
-  // Deallocazione della memoria
-  server.stop();
-  delete wifiManager;
+  // Deallocazione de
   delete led;
 }
 
-
 void loop()
 {
-  // Non c'è bisogno di implementare nulla qui perché il programma termina dopo 10 secondi
+  //server.handleClient();
 }
