b799b1c2990b713c417920c511c7bb896b6f2464
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2024-05-30 01:15:34 +0200

    fix refresh automatico della pagina

--- src/main_skatch/viewGeneric.cpp
+++ src/main_skatch/viewGeneric.cpp
@@ -7,7 +7,7 @@ String viewGeneric::defaultHeader()
      html= "<!DOCTYPE html>";
     html += "<html>";
     html += "<head>";
-    html += "<meta http-equiv='refresh' content='5'>";
+    //html += "<meta http-equiv='refresh' content='5'>";
     html += "<title>DefaultTitle</title>";
     html += "</head>";
     html += "<body>";
@@ -19,7 +19,7 @@ String viewGeneric::defaultHeader(String title)
      html= "<!DOCTYPE html>";
     html += "<html>";
     html += "<head>";
-    html += "<meta http-equiv='refresh' content='5'>";
+    //html += "<meta http-equiv='refresh' content='5'>";//refresh automatico della pagina ogni 5 sec indesiderato!
     html += "<title>";
     html += title;
     html += "</title>";
