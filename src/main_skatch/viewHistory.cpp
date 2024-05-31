eb45fcf9665caf21a97537d8d711b16d65bff4c5
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2024-06-01 00:15:08 +0200

    fix history

--- src/main_skatch/viewHistory.cpp
+++ src/main_skatch/viewHistory.cpp
@@ -7,6 +7,7 @@ String viewHistory::generateHTML()
     html = viewGeneric::defaultCssHeader("History Register");
 
     html += "<h1>History Register</h1>";
+    html += "<div class='scrollable-container' style='overflow-y: auto; max-height: 80vh;'>"; // Aggiunto stile inline per la scrollbar
     html += "<table class='history-table'>";
     html += "<tr>";
     html += "<th>Address</th>";
@@ -20,19 +21,20 @@ String viewHistory::generateHTML()
     {
         html += "<tr>";
         html += "<td>";
-        if (prec_register != SystemState::getInstance()->getAllRegisterAddress()[i]) // per tabella piu bella
+        if (prec_register != SystemState::getInstance()->getAllRegisterAddress()[i])
         {
-            html += SystemState::getInstance()->getAllRegisterAddress()[i];
+            html += String(SystemState::getInstance()->getAllRegisterAddress()[i]);
             prec_register = SystemState::getInstance()->getAllRegisterAddress()[i];
         }
         html += "</td>";
         html += "<td>";
-        html += valuesVector[i];
+        html += String(valuesVector[i]);
         html += "</td>";
         html += "</tr>";
     }
 
     html += "</table>";
+    html += "</div>";
     html += viewGeneric::defaultFooter();
 
     return html;
