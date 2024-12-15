5d5fc1d1c3ddb471f9cdc2db693ac29b5992e871
Alessio Tommasi <alessiotom@live.it>
2024-12-15 21:12:21 +0100

    .

--- docs/codeProvided/G2/Source_code/iot_project-main/src/src.ino
+++ docs/codeProvided/G2/Source_code/iot_project-main/src/src.ino
@@ -30,20 +30,28 @@ ModbusRTUSlave modbusSlave(SerialPort, DE_PIN_SLAVE);
 WebServerManager webServerManager("CONFIGURE_ME_ESP32_G2", "", master, swap);
 
 void fillHoldingRegisters(uint16_t startAddress, uint16_t quantity) {
-   
     uint16_t holdingRegistersSlave[20];
+    
+    // Inizializza i registri da 1 a 10 con il valore 100
+    for (uint16_t i = 0; i < 20; i++) {
+        if (i >= 1 && i <= 10) {
+            holdingRegistersSlave[i] = 100;
+        } else {
+            holdingRegistersSlave[i] = 0;
+        }
+    }
+    
+
     success = modbus.readHoldingRegisters(SLAVE_ID, startAddress, holdingRegistersSlave, quantity);
     if (success) {
-        if(swap){
-          for(int i = 0; i < quantity; i += 2) {
-              int temp = holdingRegistersSlave[i];
-              holdingRegistersSlave[i] = holdingRegistersSlave[i + 1];
-              holdingRegistersSlave[i + 1] = temp;
-          }
-          modbusSlave.updateHoldingRegistersSlave(holdingRegistersSlave, quantity);
-        }else{
-          modbusSlave.updateHoldingRegistersSlave(holdingRegistersSlave, quantity);
-        }    
+        if (swap) {
+            for (int i = 0; i < quantity; i += 2) {
+                int temp = holdingRegistersSlave[i];
+                holdingRegistersSlave[i] = holdingRegistersSlave[i + 1];
+                holdingRegistersSlave[i + 1] = temp;
+            }
+        }
+        modbusSlave.updateHoldingRegistersSlave(holdingRegistersSlave, quantity);
     }
 }
 
