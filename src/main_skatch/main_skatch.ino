b928b8eebfb32bc15178dbbd9978a31d7a9f5cd2
Alessio Tommasi <alessiotom@live.it>
2024-12-21 15:17:20 +0100

    add pin non funzionante causa crash in loop

--- src/main_skatch/main_skatch.ino
+++ src/main_skatch/main_skatch.ino
@@ -9,16 +9,15 @@
 #include <vector>
 #include <string>
 #include "WiFiManager.h"
-
+#include "PinoutData.h"
+#include "Esp32_38pin.h"
 
 const char *ssid = "HomeInternet";
 const char *password = "occhioallapennachecade";
 
 SystemState *systemState;
 
-SlaveModbus *slaveModbus;
-
-MasterModbus *masterModbus;
+PinoutData *pinoutData;
 
 void setup()
 {
@@ -27,32 +26,39 @@ void setup()
   {
     ; // Attendi finché la porta seriale non è pronta
   }
-
-  pinMode(32, OUTPUT);   // Set GPIO 32 as an output
-  digitalWrite(32, LOW); // Set initial state for GPIO 32
-  
+  Serial.println("Inizializzazione porta seriale effettuata!");
+  //delay(3000);
   try
   {
     systemState = SystemState::getInstance();
-    // Initialize the SlaveModbus with the correct pins
+
     
-    masterModbus = new MasterModbus();
     //WiFiManager *wifiManager = new WiFiManager(ssid, password); // per comodita versione finale usa quello sotto
     WiFiManager *wifiManager = new WiFiManager();
+    pinoutData = new Esp32_38pin();
+
+    
+
 
     systemState->setWifiManager(wifiManager);
+    systemState->setPinoutData(pinoutData);
 
-    slaveModbus = new SlaveModbus(32, 33);
+    //slaveModbus = new SlaveModbus(32, 33);
   }
   catch (const std::exception &e)
   {
-    Serial.println("Errore durante la connessione alla rete Wi-Fi! o master modbus!");
+    Serial.println("Errore durante la connessione alla rete Wi-Fi!");
   }
+
+  Serial.println("Inizializzazione completata!");
 
   while (SystemState::isStopped == false && systemState->getState() != State::ERROR)
   {  
-    delay(1000); // controllo ogni secondo se devo fermarmi
+    //Serial.println("Updating state...");
+    systemState->update();
+    delay(5000); // controllo ogni secondo se devo fermarmi
   }
+  Serial.println("System stopped!");
 
   destroy();
 }
@@ -65,6 +71,6 @@ void loop(){
 void destroy()
 {
   // delete slaveModbus;
-  delete masterModbus;
-  delete systemState;
+  //delete masterModbus;
+  //delete systemState;
 }
